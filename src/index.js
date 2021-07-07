b5f102e878b774ca8aca646d92667ca5f1eec5f6
rajesh <rk99884123@gmail.com>
2021-07-07 17:07:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { data } from './fakeDB/data.js';
+
+const App = () => {
+    const [ value, setValue ] = useState(0);
+
+    return(
+        <div className= "container">
+            <img src = { data[value].image } alt={ data[value].id } />
+            <h2>{ data[value].name}</h2>
+            <div className= "slider-actions">
+                <button type="button" onClick={() => value === 0 ? setValue(data.length - 1) : setValue(value - 1)}>
+                    <img src="./assets/images/left-arrow.png" alt="left-arrow" />
+                </button>
+                <button type="button" onClick={() => value === (data.length - 1) ? setValue(0) : setValue(value + 1)}>
+                    <img src="./assets/images/right-arrow.png" alt="right-arrow" />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
